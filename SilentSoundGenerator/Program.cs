de41e3e333eb9834f3290a31c9e4357ab5577779
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2024-08-31 19:14:31 +0200

    Add project files.

---
+++ SilentSoundGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿using NAudio.Wave;
+
+var waveOut = new WaveOutEvent();
+waveOut.Init(new SilenceProvider(new WaveFormat(44100, 1)).ToSampleProvider());
+waveOut.Play();
+
+while(true)
+{
+	await Task.Delay(1000);
+}
